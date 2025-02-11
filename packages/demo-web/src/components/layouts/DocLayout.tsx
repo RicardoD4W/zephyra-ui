d2d0628b714fe18ee5c1e0caca8f49948a165715
ricardo <ricardodeveloper03@gmail.com>
2025-02-11 16:08:48 +0100

    ci(demo-web): Added extensions

--- packages/demo-web/src/components/layouts/DocLayout.tsx
+++ packages/demo-web/src/components/layouts/DocLayout.tsx
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 
 export const DocLayout = ({ children }) => {
   useEffect(() => {
-    import('../../../../components/z-button/dist/z-button');
-    import('../../../../components/z-alert/dist/z-alert');
-    import('../../../../components/z-icon/dist/z-icon');
-    import('../../../../components/z-toggle-theme/dist/z-toggle-theme');
-    import('../../../../components/z-color/dist/z-color');
+    import('../../../../components/z-button/dist/z-button.js');
+    import('../../../../components/z-alert/dist/z-alert.js');
+    import('../../../../components/z-icon/dist/z-icon.js');
+    import('../../../../components/z-toggle-theme/dist/z-toggle-theme.js');
+    import('../../../../components/z-color/dist/z-color.js');
   }, []);
 
   return <>{children}</>;
